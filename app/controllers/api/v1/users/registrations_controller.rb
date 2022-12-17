a25fdda9f1319f2ae43822da1a45036858616fb1
natapapon.r <natapapon.r@gmail.com>
2022-12-17 20:50:23 +0700

    fix register

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Users::RegistrationsController < Api::V1::ApplicationV1Controller
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       render json: { user: @user }, status: :ok
     else
@@ -13,7 +13,7 @@ class Api::V1::Users::RegistrationsController < Api::V1::ApplicationV1Controller
     end
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:email, :password)
